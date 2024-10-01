1c6ff951aae804f2679a98c80a85fd20ebc71254
Ken Richards <kenrichards84@gmail.com>
2024-10-01 16:40:34 -0600

    Removing testing code.

--- web/chars.php
+++ web/chars.php
@@ -29,39 +29,6 @@ $pdo = new PDO('mysql:dbname=cougar_tutorial;host=127.0.0.1', "root");
 // $pdo = new PDO('oci:dbname=//cman-dev.byu.edu:31200/cescpy1.byu.edu', "name", "password");
 // $pdo = PDOFactory::getConnection("ces");
 
-// https://www.php.net/manual/en/pdo.constants.php#pdo.constants.attr-connection-status
-// echo '<div> ATTR_DEFAULT_STR_PARAM   : ' . $pdo->getAttribute(PDO::ATTR_DEFAULT_STR_PARAM ) . '</div>';
-
-
-
-// $select = $pdo->query("select SimpleString, Count, id, LongString, count(*) from chartest where id = 1");
-// $meta = $select->getColumnMeta(0);
-
-// echo '<div>getColumnMeta: </div>';
-// var_dump($meta);
-
-// $update = $pdo->prepare("update chartest set count = 2 where id = 1");
-// $update->execute();
-
-// echo '<div> update getAttribute(count): </div>';
-// $meta = $update->getColumnMeta(0);
-// var_dump($meta);
-
-// $sql = "SELECT SCHEMA_NAME 'database', default_character_set_name 'charset', DEFAULT_COLLATION_NAME 'collation' FROM information_schema.SCHEMATA";
-// $result = $pdo->query($sql);
-
-// echo '<div> schema_name rowcount: ' . $result->rowCount() .'</div>';
-// print_r($result);
-
-// echo '<div> database   charset   collation: </div>';
-// foreach ($result as $row)
-// {
-//      echo '<div> ' . $row['database'] . " " . $row['charset'] . " " . $row['collation'] .  "</div>";
-// }
-
-
-
-
 // // Create a new UserPdo object
 $simple = new CharTestPdo($security, $cache, $pdo, null,
                           null, true, "unknown");
